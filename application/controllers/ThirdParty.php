2ed4997626da6374140053fc8d82ee4982ae9ebd
jokevin <jokevin.rachmadi@gmail.com>
2021-10-11 13:49:50 +0700

    midtrans cancel

--- application/controllers/ThirdParty.php
+++ application/controllers/ThirdParty.php
@@ -114,6 +114,7 @@ class ThirdParty extends CI_Controller
 			file_put_contents('/home/logs/midtrans'.date('Ymd').'.log','notification at '.date('Y-m-d H:i:s').PHP_EOL,FILE_APPEND);
 			file_put_contents('/home/logs/midtrans'.date('Ymd').'.log',json_encode($input).PHP_EOL,FILE_APPEND);
 			$transaction_id = $input['transaction_id'];
+			var_dump($input);
 			if(substr($input['status_code'],0,1)=="2"){
 				$this->midtrans_model->approving_transaction($input['transaction_id']);
 			}
